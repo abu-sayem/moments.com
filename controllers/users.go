bbabb0d36f21b9c38276c9e6076f126b4ea3891d
abu-sayem <abusaayem@gmail.com>
2020-11-21 20:35:02 +0600

    Wrote a rough first draft of code to parse the signup form

---
+++ controllers/users.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"fmt"
+	"moments.com/views"
+	"net/http"
+)
+func NewUsers() *Users {
+	return &Users{
+		 NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+type Users struct {
+	NewView *views.View
+}
+
+// GRT /signup
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	u.NewView.Render(w, nil)
+}
+
+//POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, r.PostForm["email"])
+	fmt.Fprintln(w, r.PostForm["password"])
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"moments.com/controllers"
 	//"html/template"
 	"moments.com/views"
 	"net/http"
@@ -10,7 +11,6 @@ import (
 var (
 	homeView *views.View
 	contactView *views.View
-	signupView *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -24,20 +24,16 @@ func contact(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
-}
-
 func main() {
 	homeView = views.NewView("bootstrap","views/home.gohtml")
 	contactView = views.NewView("bootstrap","views/contact.gohtml")
-	signupView = views.NewView("bootstrap","views/signup.gohtml")
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/contact", contact).Methods("GET")
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
+	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	http.ListenAndServe(":3000", r)
 
 }
