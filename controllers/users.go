720b007600a36e4148ac76b78009aff17b4e318a
abu-sayem <abusaayem@gmail.com>
2020-11-22 15:44:22 +0600

    Added in the scema package and used it to parse signup form

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"github.com/gorilla/schema"
 	"fmt"
 	"moments.com/views"
 	"net/http"
@@ -20,11 +21,22 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	u.NewView.Render(w, nil)
 }
 
+type SignupForm struct {
+	Email string `schema: "email"`
+	Password string `svhema: "password"`
+}
+
 //POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		panic(err)
 	}
+	dec := schema.NewDecoder()
+	var form SignupForm
+	if err := dec.Decode(&form, r.PostForm); err !=nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 	fmt.Fprintln(w, r.PostForm["email"])
 	fmt.Fprintln(w, r.PostForm["password"])
 }
