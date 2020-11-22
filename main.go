2cbf40cc0f13b4dab0429468f086608f6441bfd7
abu-sayem <abusaayem@gmail.com>
2020-11-22 23:04:32 +0600

    Created a controller for static pages

--- main.go
+++ main.go
@@ -2,36 +2,16 @@ package main
 
 import (
 	"moments.com/controllers"
-	//"html/template"
-	"moments.com/views"
 	"net/http"
 	"github.com/gorilla/mux"
 )
 
-var (
-	homeView *views.View
-	contactView *views.View
-)
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-
-}
-
 func main() {
-	homeView = views.NewView("bootstrap","views/home.gohtml")
-	contactView = views.NewView("bootstrap","views/contact.gohtml")
 	usersC := controllers.NewUsers()
-
+	staticC := controllers.NewStatic()
 	r := mux.NewRouter()
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/contact", contact).Methods("GET")
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	http.ListenAndServe(":3000", r)
